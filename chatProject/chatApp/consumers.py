1fd43488eeaf333561eb56b70c232fe4b783718c
Byoung-Wha Cho <whqudghk28@gmail.com>
2024-03-01 16:48:45 +0900

    feat: get only 'w', 's' input

--- chatProject/chatApp/consumers.py
+++ chatProject/chatApp/consumers.py
@@ -13,4 +13,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        print(f"Received key press: {message}")
+        # 클라이언트로부터 'w' 또는 's' 입력 받기
+        if message == 'w':
+            print("Move up")
+            # 필요한 경우 클라이언트로 응답 보내기
+            await self.send(text_data=json.dumps({
+                'message': 'Moving up'
+            }))
+        elif message == 's':
+            print("Move down")
+            # 필요한 경우 클라이언트로 응답 보내기
+            await self.send(text_data=json.dumps({
+                'message': 'Moving down'
+            }))
+        # text_data_json = json.loads(text_data)
+        # message = text_data_json['message']
+
+        # print(f"Received key press: {message}")
